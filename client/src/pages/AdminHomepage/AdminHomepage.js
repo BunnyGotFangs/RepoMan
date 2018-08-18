1e2ad90f4c99002c86b00eb2566868c230ebf7ea
Risa Piper <risapiper@er-10-129-188-18.wireless.umn.edu>
2018-08-18 13:48:19 -0500

    more working styles

--- client/src/pages/AdminHomepage/AdminHomepage.js
+++ client/src/pages/AdminHomepage/AdminHomepage.js
@@ -25,23 +25,9 @@ class Card extends Component {
   render() {
     return (
       <div>
-        <button onClick={this.showMenu}>
-          Click to select your role
-        </button>
-        
-        {
-          this.state.showMenu
-            ? (
-              <div className="menu">
-                <button> Partner </button>
-                <button> User </button>
-                <button> Admin </button>
-              </div>
-            )
-            : (
-              null
-            )
-        }
+        <button onClick={this.state.showMenu}> Partner <a href="http://localhost:3000/admin/partners"></a> </button>
+        <button onClick={this.state.showMenu}> User <a href="http://localhost:3000/admin/users"></a> </button>
+        <button onClick={this.state.showMenu}> Role <a href="http://localhost:3000/admin/roles"></a> </button>
       </div>
     );
   }
@@ -52,63 +38,3 @@ export default Card;
 
 
 
-
-
-/////  Kim's original code ///
-// import axios from 'axios';
-// import React, { Component, Fragment } from 'react';
-// import { List, ListItem } from 'material-ui/List';
-// import { withUser } from '../services/withUser';
-
-// class HomePage extends Component {
-//   state = {
-//     stuff: null
-//   }
-//   componentDidMount() {
-//     // only try loading stuff if the user is logged in.
-//     if (!this.props.user) {
-//       return;
-//     }
-
-//     axios.get('/api/stuff')
-//       .then(res => {
-//         this.setState({
-//           stuff: res.data
-//         });
-//       })
-//       .catch(err => {
-//         // if we got an error, we'll just log it and set stuff to an empty array
-//         console.log(err);
-//         this.setState({
-//           stuff: []
-//         });
-//       });
-//   }
-//   render() {
-//     const { user } = this.props; // get the user prop from props
-//     const { stuff } = this.state; // get stuff from state
-
-//     return (
-//       <Fragment>
-//         {user && stuff &&
-//           <div>
-//             Welcome back, {user.username}!
-//           <List>
-//            {stuff.map((s, i) => <ListItem key={i} primaryText={s} />)}
-//           </List>
-//           </div>
-//         }
-//         {user && !stuff &&
-//           <div>Hold on, looking for your stuff...</div>
-//         }
-//         {!user &&
-//           <div>Hey! I don't recognize you! Register and log in using the link above</div>
-//         }
-//       </Fragment>
-//     );
-//   }
-// }
-
-// // withUser function will wrap the specified component in another component that will
-// // inject the currently logged in user as a prop called "user"
-// export default withUser(HomePage);
